117165318eac16a47b7ff300997c7d75b774c4ff
Kiko Climent <climent.kiko@gmail.com>
2024-01-27 16:49:16 +0000

    Finish size logic

--- shopping_cart/contexts.py
+++ shopping_cart/contexts.py
@@ -28,7 +28,7 @@ def shopping_cart_contents(request):
                 product_count += quantity
                 shopping_cart_items.append({
                 'item_id': item_id,
-                'quantity': item_data,
+                'quantity': quantity,
                 'product': product,
                 'size': size,
             })
