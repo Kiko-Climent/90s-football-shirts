ea0d8231dbc8bd00f0457841b6ed44932337b7cc
Kiko Climent <climent.kiko@gmail.com>
2024-01-29 08:55:56 +0000

    Add subtotal calculation

--- shopping_cart/templatetags/shopping_cart_tools.py
+++ shopping_cart/templatetags/shopping_cart_tools.py
@@ -1,7 +1,7 @@
-#from django import template
+from django import template
 
-#register = template.Library()
+register = template.Library()
 
-#@register.filter(name='calc_subtotal')
-#def calc_subtotal(price, quantity):
-#    return price * quantity
+@register.filter(name='calc_subtotal')
+def calc_subtotal(price, quantity):
+    return price * quantity
