624031bec672d4d4db68706315ec076b969f6d9b
Kiko Climent <climent.kiko@gmail.com>
2024-01-29 14:19:45 +0000

    Success message triggers once adding products to cart

--- shopping_cart/views.py
+++ shopping_cart/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+
+from products.models import Product
 
 
 def view_shopping_cart(request):
@@ -6,8 +9,10 @@ def view_shopping_cart(request):
 
 def add_to_shopping_cart(request, item_id):
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    
     size = None
     if 'product_size' in request.POST:
         size = request.POST['product_size']
@@ -17,17 +22,20 @@ def add_to_shopping_cart(request, item_id):
         if item_id in list(shopping_cart.keys()):
             if size in shopping_cart[item_id]['items_by_size'].keys():
                 shopping_cart[item_id]['items_by_size'][size] += quantity
+                
             else:
-                shopping_cart[item_id]['items_by_size'][size] = quantity     
+                shopping_cart[item_id]['items_by_size'][size] = quantity    
         else:
             shopping_cart[item_id] = {'items_by_size': {size: quantity}}
+            print(f'Message to be displayed: Added {product.team} to your cart')
+            messages.success(request, f'Added {product.team} to your cart')
     else:       
 
         if item_id in list(shopping_cart.keys()):
             shopping_cart[item_id] += quantity
         else:
             shopping_cart[item_id] = quantity
-
+    
     request.session['shopping_cart'] = shopping_cart
     return redirect(redirect_url)
 
