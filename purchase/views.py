02529574d894f30912043035e98257909cc3e9df
Kiko Climent <climent.kiko@gmail.com>
2024-01-31 14:24:17 +0000

    Add basic purchase functionality

--- purchase/views.py
+++ purchase/views.py
@@ -1,20 +1,40 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from shopping_cart.contexts import shopping_cart_contents
+
+import stripe
 
 def purchase_view(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
     shopping_cart = request.session.get('shopping_cart', {})
     if not shopping_cart:
         messages.error(request, 'Your shopping cart is empty')
         return redirect(reverse('products'))
     
+    current_shopping_cart = shopping_cart_contents(request)
+    total = current_shopping_cart['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
     template = 'purchase/purchase.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51OZ8GyDgPKCOcRaNCflIxug6zGANzV3pYCVm4jp7Jczy9bcvD1dW4NFFMx8q6TTrCX0wq4eYmJ0HY2mTsJ3IXT1M00dVJzynkd',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
