656a32e569247418895b267232090181db8f9d46
Kiko Climent <climent.kiko@gmail.com>
2024-02-01 11:34:32 +0000

    Add webhooks - class and views

--- purchase/urls.py
+++ purchase/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path('', views.purchase_view, name='purchase'),
     path('purchase_success/<order_number>', views.purchase_success, name='purchase_success'),
+    path('wh/', webhook, name='webhook'),
 ]
