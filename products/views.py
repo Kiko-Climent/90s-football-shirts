6ccb283344b844053ae4b7f2b9f81b680d360707
Kiko Climent <climent.kiko@gmail.com>
2024-02-03 23:41:34 +0000

    Product Admin - Edit product functionality

--- products/views.py
+++ products/views.py
@@ -97,3 +97,26 @@ def add_product(request):
     }
 
     return render(request, template, context)
+
+def edit_product(request, product_id):
+
+    product = get_object_or_404(Product, pk=product_id)
+    form = ProductForm(instance=product)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Product succesfully updated')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Error by updating this product, please check the form')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, 'You are about to edit the product: { product.team }')
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)
