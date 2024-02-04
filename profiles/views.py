ccd7048d80759b580a1cf4bf3c8313e7bf4fe03c
Kiko Climent <climent.kiko@gmail.com>
2024-02-04 15:25:24 +0000

    Add login decorator to restrict actions

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 from .models import UserProfile
 from .forms import UserProfileForm
 
 from purchase.models import Order
 
+@login_required
 def profile(request):
     """ A view for the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
