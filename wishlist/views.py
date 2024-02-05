ad54658be33c51d063163670c0d1b8033f0f7318
Kiko Climent <climent.kiko@gmail.com>
2024-02-05 22:58:04 +0000

    Wishlist - add logic when item is already in the users wishlist

--- wishlist/views.py
+++ wishlist/views.py
@@ -18,11 +18,16 @@ def wishlist(request):
 
 @login_required
 def add_to_wishlist(request, item_id):
-    product = get_object_or_404(Product, pk=item_id)
+    product= get_object_or_404(Product, pk=item_id)
     wishlist, created = Wishlist.objects.get_or_create(user=request.user)
-    wishlist.products.add(product)
-    messages.success(request, f'Added {product.team} to your wishlist.')
-    return redirect('product_detail', product_id=item_id)
+
+    if wishlist.products.filter(pk=item_id).exists():
+        messages.info(request, f'{product.team} is already in your wishlist.')
+        return redirect('product_detail', product_id=item_id)
+    else:
+        wishlist.products.add(product)
+        messages.success(request, f'Added {product.team} to your wishlist.')
+        return redirect('product_detail', product_id=item_id)
 
 @login_required
 def remove_from_wishlist(request, item_id):
