676d04c61bb5acbc008e6420785e4b222c57b282
Kiko Climent <climent.kiko@gmail.com>
2024-02-08 23:11:24 +0000

    Contatc -Fix email functionality, style template

--- contact/models.py
+++ contact/models.py
@@ -9,8 +9,11 @@ class Inquiry(models.Model):
 
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=254, blank=False)
-    subject_type = models.CharField(max_length=10, choices=INQUIRY_CHOICES, default='GENERAL', blank=False)
-    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, blank=True)
+    subject_type = models.CharField(
+        max_length=10, choices=INQUIRY_CHOICES,
+        default='GENERAL', blank=False)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL,
+                                 null=True, blank=True)
     message = models.TextField(blank=False)
     timestamp = models.DateTimeField(auto_now_add=True)
 
