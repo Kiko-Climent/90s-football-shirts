bcc7d4953623d0646d26a6021c1fafd1cc003d8a
Kiko Climent <climent.kiko@gmail.com>
2024-02-11 16:18:13 +0000

    Deploy to test emails

--- nineties_football_shirts/settings.py
+++ nineties_football_shirts/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-kikocliment-90sfootball-pv0ptcxcw9y.ws-eu108.gitpod.io', 'nineties-football-shirts-8ef65f488fe9.herokuapp.com']
 
